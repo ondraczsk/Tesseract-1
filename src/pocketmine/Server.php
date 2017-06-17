dc1ba12bb0b0f5223c161fcf88190525ceb7fc80
Apple <ytapplegamer@gmail.com>
2017-06-17 00:37:51 -0400

    Forgot the hyphen

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1682,7 +1682,7 @@ class Server{
 			$this->levelMetadata = new LevelMetadataStore();
 
 			$this->operators = new Config($this->dataPath . "ops.txt", Config::ENUM);
-			$this->whitelist = new Config($this->dataPath . "whitelist.txt", Config::ENUM);
+			$this->whitelist = new Config($this->dataPath . "white-list.txt", Config::ENUM);
 			if(file_exists($this->dataPath . "banned.txt") and !file_exists($this->dataPath . "banned-players.txt")){
 				@rename($this->dataPath . "banned.txt", $this->dataPath . "banned-players.txt");
 			}
