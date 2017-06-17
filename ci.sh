b669323d43fa63deeebf44bf33052186289a4365
Apple <ytapplegamer@gmail.com>
2017-06-17 12:22:00 -0400

    Fix the unknown command in ci.sh

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ for file in **/*.php; do
     [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
 echo Lint done successfully.
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
+echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
 if ls plugins/Tesseract/Tesseract*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
